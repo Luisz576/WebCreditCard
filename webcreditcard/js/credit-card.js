f99b7dd9328aa9053825067831035d665b63e5e0
Luisz576 <lrferlinalves@gmail.com>
2023-02-07 09:47:10 -0300

    verify logic

--- webcreditcard/js/credit-card.js
+++ webcreditcard/js/credit-card.js
@@ -1,3 +1,20 @@
+class CreditType{
+    constructor(name, validate){
+        this.name = name
+        this.validate = validate
+        // this.image = image
+    }
+}
+
+const CreditTypes = [
+    new CreditType('Visa', /^4[0-9]{6,}$/),
+    new CreditType('MasterCard', /^5[1-5][0-9]{5,}|222[1-9][0-9]{3,}|22[3-9][0-9]{4,}|2[3-6][0-9]{5,}|27[01][0-9]{4,}|2720[0-9]{3,}$/),
+    new CreditType('AmericanExpress', /^3[47][0-9]{5,}$/),
+    new CreditType('DinersClub', /^3(?:0[0-5]|[68][0-9])[0-9]{4,}$/),
+    new CreditType('Discover', /^6(?:011|5[0-9]{2})[0-9]{3,}$/),
+    new CreditType('JBC', /^(?:2131|1800|35[0-9]{3})[0-9]{3,}$/),
+]
+
 function initCreditCardListeners(){
     const creditCard = document.getElementById('credit-card')
     const ccNumberF = document.getElementById("credit-card-form-number")
@@ -18,11 +35,95 @@ function initCreditCardListeners(){
         ccValidF.addEventListener('focus', removeClass)
         ccNameF.addEventListener('focus', removeClass)
         ccNumberF.addEventListener('focus', removeClass)
+
+        function getCreditType(number){
+            if(number && number.trim() != ''){
+                number = number.trim().replaceAll(' ', '')
+                for(i in CreditTypes){
+                    if(CreditTypes[i].validate.test(number.trim())){
+                        return CreditTypes[i]
+                    }
+                }
+            }
+            return new CreditType('None', /^$/)
+        }
+
+        function formatNumber(form, component){
+            let value = form.value
+            if(!value || value.trim() == ''){
+                value = '0000 0000 0000 0000'
+            }else{
+                const valueClear = value.trim().replaceAll(' ', '')
+                value = ''
+                for(i in valueClear){
+                    if(!isNaN(parseInt(valueClear[i]))){
+                        value += parseInt(valueClear[i])
+                    }
+                }
+                const parts = [value.substring(0, 4), value.substring(4, 8), value.substring(8, 12), value.substring(12, 16)]
+                value = ''
+                for(i in parts){
+                    if(parts[i] && parts[i].trim() != ''){
+                        if(i != 0)
+                            value += ' '
+                        value += parts[i].trim()
+                    }
+                }
+                form.value = value
+            }
+            component.innerText = value
+        }
+        function formatName(form, component){
+            //TODO
+        }
+        function formatValid(form, component){
+            let value = form.value
+            if(!value || value.trim() == ''){
+                value = '00/00'
+            }else{
+                const valueClear = value.trim().replaceAll(' ', '').replaceAll('/', '')
+                value = ''
+                for(i in valueClear){
+                    if(!isNaN(parseInt(valueClear[i]))){
+                        value += valueClear[i]
+                    }
+                }
+                const m = value.substring(0, 2)
+                const y = value.substring(2, 4)
+                if(m > 0 && m < 13){
+                    if(m < 10){
+                        m = '0' + m
+                    }
+                    value = m + '/'
+                    const pattern = /^\d{2}$/
+                    if(pattern.test(y)){
+                        value += y
+                    }
+                }
+                value = m + '/' + y
+                form.value = value
+            }
+            if(value == '00/00')
+                value = ''
+            component.innerText = value
+        }
+        function formatCode(form, component){
+            let value = form.value
+            if(!value || value.trim() == ''){
+                value = '000'
+            }else{
+                value = value.substring(0, 3)
+                form.value = value
+            }
+            component.innerText = value
+        }
+
         document.addEventListener('keyup', (_) => {
-            ccNumber.innerText = ccNumberF.value == '' ? '0000 0000 0000 0000' : ccNumberF.value
-            ccName.innerText = ccNameF.value == '' ? 'No Name' : ccNameF.value
-            ccValid.innerText = ccValidF.value == '' ? '00/00' : ccValidF.value
-            ccCode.innerText = ccCodeF.value == '' ? '000' : ccCodeF.value
+            formatNumber(ccNumberF, ccNumber)
+            formatName(ccNameF, ccName)
+            formatValid(ccValidF, ccValid)
+            formatCode(ccCodeF, ccCode)
+            const creditCardType = getCreditType(ccNumberF.value)
         })
         return
     }
